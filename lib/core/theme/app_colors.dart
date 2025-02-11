1e62a18335f857ba51e1c0811bcc92a2eb348e0d
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2025-02-11 10:46:59 +0100

    in progress

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color.fromARGB(255, 142, 151, 253);
+  static const secondary = Color.fromARGB(255, 117, 131, 202);
+  static const backgroundColor = Color(0xFFFFFF);
+}
---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:silent_moon/core/theme/app_colors.dart';
+
+class AppTheme {
+  static final theme = ThemeData(
+    primaryColor: AppColors.primary,
+  );
+}
