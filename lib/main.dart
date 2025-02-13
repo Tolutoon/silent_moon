00772a525b157ba27068bc082e1652743a1db4e9
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2025-02-13 15:26:33 +0100

    routing in progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:silent_moon/core/theme/app_theme.dart';
 import 'package:silent_moon/presentation/auth/sigin_signup.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+final GoRouter _router = GoRouter(routes: <RouteBase>[
+  GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) =>
+          SignInSignUpScreen())
+]);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
